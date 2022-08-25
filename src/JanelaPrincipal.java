4f0db2d88a8456f6e44c9ce0a691afd4c47b8bfe
Alex  Gabriel <alexgabriel.araujo@hotmail.com>
2022-08-25 11:59:31 -0300

    bomntio

--- src/JanelaPrincipal.java
+++ src/JanelaPrincipal.java
@@ -13,6 +13,8 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class JanelaPrincipal extends JFrame {
 
@@ -40,14 +42,14 @@ public class JanelaPrincipal extends JFrame {
 	public JanelaPrincipal() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 633, 397);
+		setBounds(100, 100, 907, 501);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(102, 205, 170));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(283, 189, 160, 55);
 		btnLogin.setForeground(new Color(0, 0, 0));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -55,13 +57,14 @@ public class JanelaPrincipal extends JFrame {
 				j2.setVisible(true);
 			}
 		});
-		btnLogin.setBackground(new Color(30, 144, 255));
+		contentPane.setLayout(null);
+		btnLogin.setBackground(new Color(0, 204, 255));
 		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 23));
-		btnLogin.setBounds(82, 157, 193, 44);
 		contentPane.add(btnLogin);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.setBackground(new Color(30, 144, 255));
+		btnCadastrar.setBounds(507, 189, 160, 55);
+		btnCadastrar.setBackground(new Color(0, 204, 255));
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				JanelaCadastrarAcesso j1 = new JanelaCadastrarAcesso();
@@ -69,8 +72,12 @@ public class JanelaPrincipal extends JFrame {
 			}
 		});
 		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 23));
-		btnCadastrar.setBounds(334, 157, 193, 44);
 		contentPane.add(btnCadastrar);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Downloads\\Pink Pastel Textured Compay Mind Map Brainstorm\\1.png"));
+		lblNewLabel.setBounds(0, 0, 891, 462);
+		contentPane.add(lblNewLabel);
 	}
 
 }
