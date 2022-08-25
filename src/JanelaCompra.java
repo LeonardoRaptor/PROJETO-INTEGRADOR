d0657f02ac68575e8cad08010202e695bcec9ce5
Leonardo Donine Mateus <ldmraptor19@gmail.com>
2022-08-25 11:51:03 -0300

    atualização 2.1

--- src/JanelaCompra.java
+++ src/JanelaCompra.java
@@ -6,10 +6,16 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class JanelaCompra extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -33,7 +39,7 @@ public class JanelaCompra extends JFrame {
 	public JanelaCompra() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 460);
+		setBounds(100, 100, 450, 175);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -41,24 +47,60 @@ public class JanelaCompra extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(333, 396, 91, 14);
+		lblNewLabel.setBounds(333, 93, 91, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Valor Total: R$");
-		lblNewLabel_1.setBounds(238, 396, 85, 14);
+		lblNewLabel_1.setBounds(238, 93, 85, 14);
 		contentPane.add(lblNewLabel_1);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		textArea.setBounds(238, 36, 186, 349);
-		contentPane.add(textArea);
 		
 		JLabel lblNewLabel_2 = new JLabel("Lista de Compras:");
 		lblNewLabel_2.setBounds(238, 11, 186, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		JButton btnNewButton = new JButton("Sair");
-		btnNewButton.setBounds(10, 392, 89, 23);
+		btnNewButton.setBounds(10, 89, 89, 23);
 		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_3 = new JLabel("IdProduto:");
+		lblNewLabel_3.setBounds(10, 64, 75, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField = new JTextField();
+		textField.setBounds(95, 61, 133, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(95, 36, 133, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Quantidade:");
+		lblNewLabel_4.setBounds(10, 39, 75, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Livro:");
+		lblNewLabel_5.setBounds(10, 11, 46, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("New label");
+		lblNewLabel_6.setBounds(66, 11, 162, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Quantidade", "Nome", "Pre\u00E7o"
+			}
+		));
+		table.setBounds(238, 84, 174, -47);
+		contentPane.add(table);
 	}
 }
