aa09a17b59c34249034babbf879b5d5a1a0bc8f0
Andrea Nogueira <nogueiraandrea816@gmail.com>
2022-09-08 09:11:01 -0300

    Coisas bonitinhas Nog

---
+++ src/Cadastro.java
@@ -0,0 +1,42 @@
+
+public class Cadastro {
+	public String nome, autor, genero;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public double getId() {
+		return id;
+	}
+	public void setId(double id) {
+		this.id = id;
+	}
+	public double getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public double id, quantidade, preco;
+
+}
