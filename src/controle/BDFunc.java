abb62a499f89ade7117f540a84740eb104d635e2
Alex <alexgabriel.araujo@hotmail.com>
2022-09-29 12:08:53 -0300

    Tela de Cadastro

--- src/controle/BDFunc.java
+++ src/controle/BDFunc.java
@@ -1,28 +1,26 @@
 package controle;
+
 //import java.util.ArrayList;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import java.util.ArrayList;
 
 import modelo.Funcionario;
-import visao.JanelaAdmin;
-import visao.JanelaLogin;
-import visao.Menu;
 
 //import modelo.Funcionario;
 public class BDFunc {
 	Connection conexao;
 	private Statement st;
-	Funcionario f = new Funcionario();
+	private ArrayList<Funcionario> lista = new ArrayList<>();
 
-	
-	public void adquirirFunc() {
+	public void adquirirFunc(Funcionario f) {
 		try {
 			ResultSet rs = st.executeQuery("select * from funcionarios");
 			while (rs.next()) {
-				f.setId(rs.getInt("id"));
+				f.setId(rs.getInt("idFuncionario"));
 				f.setCpfFunc(rs.getString("cpf"));
 				f.setEmailFunc(rs.getString("emailFunc"));
 				f.setNomeFunc(rs.getString("nomeFuncionario"));
@@ -32,79 +30,148 @@ public class BDFunc {
 		} catch (SQLException a) {
 			System.out.println(a.getMessage());
 			System.out.println("Erro ao conectar � base de dados.");
-			
+
 		}
-		
+
 	}
-	
-	public void cadastro () {
-		
+
+	public int cadastro(Funcionario f) {
+
+		int idCadastrado = 0;
 		try {
-			
+
 			Statement st;
 			conexao = Conexao.ligar();
 			st = conexao.createStatement();
-			
+
 			System.out.println("Conectado � base de dados com sucesso.");
-			st.executeUpdate("Insert into funcionarios (NomeFuncionario,EmailFunc,CPF, SenhaFunc) values "
-					+ "('" + f.getNomeFunc() + "', '" + f.getEmailFunc() + "','\" + f.getTelefone() + \"', '" + f.getCpfFunc() + "', "
-							+ "'" + f.getSenhaFunc()+ "')");
-			
+			idCadastrado = st.executeUpdate(
+					"Insert into funcionarios (NomeFuncionario,EmailFunc, telefone, CPF, SenhaFunc) values " + "('"
+							+ f.getNomeFunc() + "', '" + f.getEmailFunc() + "','" + f.getTelefone() + "', '"
+							+ f.getCpfFunc() + "', " + "'" + f.getSenhaFunc() + "')");
+
+			if (idCadastrado == 0) {
+				throw new SQLException("Creating user failed, no rows affected.");
+			}
+
 			Conexao.desligar();
-			
+
 		} catch (SQLException a) {
 			System.out.println(a.getMessage());
-				System.out.println("Erro ao conectar � base de dados.");
+			System.out.println("Erro ao conectar � base de dados.");
 		}
+
+		return idCadastrado;
 	}
-	public Funcionario logarConta(String login, String senha) {
+
+	public ArrayList<Funcionario> listarTodos() {
+
 		
+		try {
+			conexao = Conexao.ligar();
+			System.out.println("Conectado � base de dados com sucesso.");
+
+			st = conexao.createStatement();
+			ResultSet rs1 = st.executeQuery("select * from funcionarios order by NomeFuncionario");
+
+			while (rs1.next()) {
+				Funcionario f = new Funcionario();
+				f.setId(rs1.getInt("idFuncionario"));
+				f.setCpfFunc(rs1.getString("cpf"));
+				f.setEmailFunc(rs1.getString("emailFunc"));
+				f.setNomeFunc(rs1.getString("nomeFuncionario"));
+				f.setSenhaFunc(rs1.getString("senhaFunc"));
+				f.setTelefone(rs1.getString("telefone"));
+				lista.add(f);
+			}
+
+			Conexao.desligar();
+
+		} catch (SQLException a) {
+			System.out.println(a.getMessage());
+			System.out.println("Erro ao conectar � base de dados.");
+
+		}
+
+		return lista;
+	}
+
+	public Funcionario logarConta(String login, String senha) {
+
 		Funcionario funcionario = null;
 		try {
 			conexao = Conexao.ligar();
 			System.out.println("Conectado � base de dados com sucesso.");
-			
-			st = conexao.createStatement();
-			ResultSet rs1 = st.executeQuery("select * from funcionarios where senhaFunc='" + senha + "' and nomeFuncionario='" + login + "'");
 
+			st = conexao.createStatement();
+			ResultSet rs1 = st.executeQuery(
+					"select * from funcionarios where senhaFunc='" + senha + "' and nomeFuncionario='" + login + "'");
+
+			Funcionario f = new Funcionario();
 			while (rs1.next()) {
+				f.setId(rs1.getInt("idFuncionario"));
 				f.setCpfFunc(rs1.getString("cpf"));
 				f.setEmailFunc(rs1.getString("emailFunc"));
 				f.setNomeFunc(rs1.getString("nomeFuncionario"));
 				f.setSenhaFunc(rs1.getString("senhaFunc"));
 				f.setTelefone(rs1.getString("telefone"));
 			}
-			
+
 			Conexao.desligar();
-			
-		}catch(SQLException a) {
+
+		} catch (SQLException a) {
 			System.out.println(a.getMessage());
 			System.out.println("Erro ao conectar � base de dados.");
-			
+
 		}
-		
+
 		return funcionario;
 	}
-	public void acessarBd(){
+
+	public void acessarBd() {
 		try {
 			conexao = Conexao.ligar();
 			System.out.println("Conectado � base de dados com sucesso.");
-			
+
 			st = conexao.createStatement();
+
+			Conexao.desligar();
+
+		} catch (SQLException a) {
+			System.out.println(a.getMessage());
+			System.out.println("Erro ao conectar � base de dados.");
+
+		}
+	}
+
+	public void alterarFuncionario(int posicao, Funcionario funcionarioSelecionado) {
+
+	}
+
+	public ArrayList<Funcionario> removeAq() {
+		try {
+			conexao = Conexao.ligar();
+			System.out.println("Conectado � base de dados com sucesso.");
+
+			st = conexao.createStatement();
+			PreparedStatement pat = ((Connection) st)
+					.prepareStatement("delete from Funcionarios where idFuncionario=?");
+			
+			pat.executeQuery();
+			
+			
+			Funcionario f = new Funcionario();
+			lista.remove(f);
+			
 			
 			
 			Conexao.desligar();
-			
-		}catch(SQLException a) {
+
+		} catch (SQLException a) {
 			System.out.println(a.getMessage());
 			System.out.println("Erro ao conectar � base de dados.");
-			
+
 		}
-	}
-	public void alterarFuncionario(int posicao, Funcionario funcionarioSelecionado) {
-		
-		
-		
-		
+		return lista;
 	}
 }
--- src/visao/JanelaCadastrarAcesso.java
+++ src/visao/JanelaCadastrarAcesso.java
@@ -1,17 +1,16 @@
 package visao;
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.sql.*;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class JanelaCadastrarAcesso extends JFrame {
 
