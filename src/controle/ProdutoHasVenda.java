3b1d29f741fd145e6f33933d5edc2574a8ddc1d3
Leonardo Donine Mateus <ldmraptor19@gmail.com>
2022-11-28 15:34:41 -0300

    Progressos para o término

--- src/controle/ProdutoHasVenda.java
+++ src/controle/ProdutoHasVenda.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import modelo.Cliente;
+import modelo.Livro;
 import modelo.ProVenda;
 
 public class ProdutoHasVenda {
@@ -38,63 +39,75 @@ public class ProdutoHasVenda {
 
 	}
 
+	public ArrayList<ProVenda> listarTodos() {
 
-public ArrayList<ProVenda> listarTodos() {
+		try {
+			conexao = Conexao.ligar();
+			System.out.println("Conectado � base de dados com sucesso.");
 
-	try {
-		conexao = Conexao.ligar();
-		System.out.println("Conectado � base de dados com sucesso.");
+			st = conexao.createStatement();
+			ResultSet rs2 = st.executeQuery("select * from Produtos_has_Venda order by idVenda");
 
-		st = conexao.createStatement();
-		ResultSet rs2 = st.executeQuery("select * from Produtos_has_Venda order by idVenda");
+			while (rs2.next()) {
+				ProVenda pv = new ProVenda();
+				pv.setIdProduto(rs2.getInt("Produtos_idProdutos"));
+				pv.setIdVenda(rs2.getInt("Venda_idVenda"));
+				listaProV.add(pv);
+			}
 
-		while (rs2.next()) {
-			ProVenda pv = new ProVenda();
-			pv.setIdProduto(rs2.getInt("Produtos_idProdutos"));
-			pv.setIdVenda(rs2.getInt("Venda_idVenda"));
-			listaProV.add(pv);
+			Conexao.desligar();
+
+		} catch (SQLException a) {
+			System.out.println(a.getMessage());
+			System.out.println("Erro ao conectar � base de dados.");
+
 		}
 
-		Conexao.desligar();
-
-	} catch (SQLException a) {
-		System.out.println(a.getMessage());
-		System.out.println("Erro ao conectar � base de dados.");
-
+		return listaProV;
 	}
 
-	return listaProV;
-}
+	public void acessarBd() {
+		try {
+			conexao = Conexao.ligar();
+			System.out.println("Conectado � base de dados com sucesso.");
 
-public void acessarBd() {
-	try {
-		conexao = Conexao.ligar();
-		System.out.println("Conectado � base de dados com sucesso.");
+			st = conexao.createStatement();
 
-		st = conexao.createStatement();
+			Conexao.desligar();
 
-		Conexao.desligar();
+		} catch (SQLException a) {
+			System.out.println(a.getMessage());
+			System.out.println("Erro ao conectar � base de dados.");
 
-	} catch (SQLException a) {
-		System.out.println(a.getMessage());
-		System.out.println("Erro ao conectar � base de dados.");
-
-	}
-}
-
-public boolean removeAq(int idVenda) {
-	boolean sucesso = true;
-	try {
-
-		conexao = Conexao.ligar();
-		System.out.println("Conectado � base de dados com sucesso.");
-		st = conexao.createStatement();
-		sucesso = st.execute("delete from Produtos_has_Venda where idVenda=" + idVenda);
-		Conexao.desligar();
-	} catch (SQLException a) {
-		System.out.println(a.getMessage());
+		}
 	}
 
-	return sucesso;
-}
+	public int cadastroProVenda(ProVenda pv) {
+
+		int idCadastradoProVen = 0;
+		try {
+
+			Statement st;
+			conexao = Conexao.ligar();
+			st = conexao.createStatement();
+
+			System.out.println("Conectado � base de dados com sucesso.");
+			idCadastradoProVen = st
+					.executeUpdate("Insert into Produtos_has_Venda (Produtos_idProdutos,Venda_idVenda) values " + "('"
+							+ pv.getIdProduto() + "', '" + pv.getIdVenda() + "')");
+
+			if (idCadastradoProVen == 0) {
+				throw new SQLException("Creating book failed, no rows affected.");
+			}
+
+			Conexao.desligar();
+
+		} catch (SQLException a) {
+			System.out.println(a.getMessage());
+			System.out.println("Erro ao conectar � base de dados.");
+		}
+
+		return idCadastradoProVen;
+	}
+
 }
