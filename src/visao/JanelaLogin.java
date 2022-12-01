29988e8d27e30200d10c265697c8c8c05b24ccf9
nog <andrea.n1@aluno.ifsc.edu.br>
2022-12-01 11:10:51 -0300

    mudanças design

--- src/visao/JanelaLogin.java
+++ src/visao/JanelaLogin.java
@@ -116,7 +116,7 @@ public class JanelaLogin extends JFrame {
 		contentPane.add(btnLogar);
 
 		JButton btnVoltar = new JButton("Voltar");
-		btnVoltar.setForeground(Color.BLACK);
+		btnVoltar.setForeground(Color.WHITE);
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -124,7 +124,7 @@ public class JanelaLogin extends JFrame {
 				jap.setVisible(true);
 			}
 		});
-		btnVoltar.setBackground(Color.LIGHT_GRAY);
+		btnVoltar.setBackground(Color.GRAY);
 		btnVoltar.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		btnVoltar.setBounds(929, 447, 133, 35);
 		contentPane.add(btnVoltar);
